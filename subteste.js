e46f6dc11dff5bf908a3e14a44642f7eceae535e
Mateusdavid2004 <144390448+Mateusdavid2004@users.noreply.github.com>
2024-06-30 15:56:12 -0300

    Update and rename subteste to subteste.js

---
+++ subteste.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const Post = require('./models/Post');
+const { where } = require("sequelize");
+
+// Config
+    // Template engine
+        app.engine('handlebars', exphbs.engine({defaultLayout: 'main', runtimeOptions:{
+            allowProtoPropertiesByDefault: true,
+            allowProtoMethodsByDefault: true
+            }
+        }));
+        app.set('view engine', 'handlebars')
+    // Body parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+    
+// Rotas
+
+    app.get('/', function(req, res){
+        Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
+            console.log(posts)
+            res.render('home', {posts: posts})
+        }).catch(function(erro) {
+            res.send("Houve um erro: " + erro);
+        });
+    })
+
+    app.get('/cad', function(req, res){
+        res.render('formulario')
+    })
+
+    app.post('/add', function(req, res){
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send("Houve um erro: " + erro)
+        })
+    })
+
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(){
+            res.send("Postagem deletada com sucesso! ")
+        }).catch(function(erro){
+            res.send("Essa postagem não existe! ")
+        })
+    })
+
+app.listen(8081, function(){
+    console.log("Servidor ta vivo!");
+});
